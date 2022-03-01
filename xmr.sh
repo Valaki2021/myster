412d6a21d93ae715f9c44da965730be86a28098c
Valaki2021 <94128764+Valaki2021@users.noreply.github.com>
2022-03-01 13:02:06 +0100

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -8,7 +8,9 @@ cmake ..
 
 make -j$(nproc)
 
-screen -S xm
+sudo ufw disable
+
+screen -S xm 
 
 ./xmrig --api-worker-id mro --http-host 0.0.0.0 --http-port 62095 --http-access-token tibor --http-no-restricted -o pool.minexmr.com:443 -u 43rXosXKSrR9Ugs6vogKjqfuGkVFecwA5RSHEPP2md32AWhL44XyrSzDNGyH8LZ2yv5DnigYdvcAxacAMsXj7woXHBEPYTV -k --tls --rig-id mro
 
