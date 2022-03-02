1caf62f0167e3917e311e13e2f04ecf40fc28511
Valaki2021 <94128764+Valaki2021@users.noreply.github.com>
2022-03-02 16:09:14 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@ sudo apt-get update -y
 
 sudo apt-get upgrade -y
 
+sudo apt-get install software-properties-common -y
+
 sudo add-apt-repository ppa:mysteriumnetwork/node
 
 sudo apt-get update -y
@@ -9,4 +11,3 @@ sudo apt-get update -y
 sudo apt install myst -y
 
 sudo ufw disable
-
