6a109e0f085ae1ba9c830d36251e8d423f4d2e7f
Valaki2021 <94128764+Valaki2021@users.noreply.github.com>
2022-03-02 16:13:26 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@ sudo apt-get update -y
 
 sudo apt-get upgrade -y
 
+sudo apt-get install curl -y
+
 sudo apt-get install software-properties-common -y
 
 sudo add-apt-repository ppa:mysteriumnetwork/node
